bb3d85ae0c2707c6806b8affc1fc32c09671b2a4
Cosmocath <catherine.hermary@gmail.com>
2021-09-03 15:00:07 +0200

    creation des routes de bases et des fonctionnalites des boutons

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SigninComponent } from './components/signin/signin.component';
+import { LoginComponent } from './components/login/login.component';
+import { MapComponent } from './components/map/map.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component: LoginComponent,pathMatch:'full'},
+  {path:"login",component: LoginComponent,pathMatch:'full'},
+  {path:"map",component: MapComponent}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
