9e0ed382434577c13db5689210f9c31f3b177a51
AlbanRenahy <albanrenahy.pro@gmail.com>
2021-09-12 16:32:06 +0200

    feat(burger-nav.controller): styling and tooggle nav on map

---
+++ src/app/components/burger-nav/burger-nav.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BurgerNavComponent } from './burger-nav.component';
+
+describe('BurgerNavComponent', () => {
+  let component: BurgerNavComponent;
+  let fixture: ComponentFixture<BurgerNavComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ BurgerNavComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BurgerNavComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
