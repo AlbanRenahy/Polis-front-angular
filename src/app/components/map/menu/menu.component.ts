d3e7bfd6bb13314fd211109c36067e26ea1f2d6a
Cosmocath <54315376+Cosmocath@users.noreply.github.com>
2021-09-17 13:35:06 +0200

    Revert "Fix mini bug"

--- src/app/components/map/menu/menu.component.ts
+++ src/app/components/map/menu/menu.component.ts
@@ -1,23 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { RenseignementDonneesComponent } from '../renseignement-donnees/renseignement-donnees.component';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
-
-
-
-
 export class MenuComponent implements OnInit {
-  
+
   constructor() { }
 
   ngOnInit(): void {
   }
-
-
-  
 
 }
