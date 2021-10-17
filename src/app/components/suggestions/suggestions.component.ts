0e1e870690a972ed5dc94eede72363209b102f68
Cosmocath <catherine.hermary@gmail.com>
2021-10-17 19:01:25 +0200

    debug

--- src/app/components/suggestions/suggestions.component.ts
+++ src/app/components/suggestions/suggestions.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, NgModule, OnInit } from '@angular/core';
 import { Categorie } from 'src/app/shared/models/categorie';
 import { Lieu } from 'src/app/shared/models/lieu';
 import { LieuService } from 'src/app/shared/services/lieu.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-suggestions',
   templateUrl: './suggestions.component.html',
   styleUrls: ['./suggestions.component.scss']
 })
+
+
+
 export class SuggestionsComponent implements OnInit {
-  lieux: Lieu[] = [];
+  @Input() lieux: Lieu[] = [];
+  @Input() lieu!:Lieu;
 
 
   constructor(private lieuService: LieuService) { }
@@ -17,6 +22,7 @@ export class SuggestionsComponent implements OnInit {
   ngOnInit(): void {
     this.lieuService.getAll().subscribe((lieux : Lieu[])=> {
       this.lieux= lieux;
+      
       
       
     })
