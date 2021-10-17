479897e8b8bc5cf0ddf95ee22dc8a0413f50ba3a
Cosmocath <catherine.hermary@gmail.com>
2021-10-17 17:37:57 +0200

    affichage cat et temps

--- src/app/components/map/renseignement-donnees/renseignement-donnees.component.ts
+++ src/app/components/map/renseignement-donnees/renseignement-donnees.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Categorie } from 'src/app/shared/models/categorie';
 import { CategorieService } from 'src/app/shared/services/categorie.service';
 /**imports formulaire */
 import { FormBuilder,FormControl,FormGroup,Validators } from '@angular/forms';
 import { LieuService } from 'src/app/shared/services/lieu.service';
 import { Lieu } from 'src/app/shared/models/lieu';
+import { GooglePlaceDirective } from 'ngx-google-places-autocomplete';
 
 @Component({
   selector: 'app-renseignement-donnees',
@@ -13,14 +14,29 @@ import { Lieu } from 'src/app/shared/models/lieu';
 })
 export class RenseignementDonneesComponent implements OnInit {
   formulaire : FormGroup;
+  adresse: string = ''
+  latitude: string = ''
+  longitude: string = ''
+
+  @ViewChild("placesRef")
+  placesRef!: GooglePlaceDirective;
+
+  handleAddressChange(address: any) {
+    this.adresse = address.formatted_address
+    this.latitude = address.geometry.location.lat()
+    this.longitude = address.geometry.location.lng()
+  }
  
   categories: Categorie[] = [];
+
   constructor(private categorieService: CategorieService,private fb: FormBuilder, private lieuService:LieuService) {
     this.formulaire= this.fb.group({
       nomLieu: new FormControl(),
       adresse: new FormControl(),
       description: new FormControl(),
       ville: new FormControl(),
+      latitude:new FormControl(),
+      longitude:new FormControl(),
       
 
     })
@@ -29,6 +45,7 @@ export class RenseignementDonneesComponent implements OnInit {
   ngOnInit(): void {
     this.categorieService.getAll().subscribe((categories : Categorie[])=> {
       this.categories= categories;
+      
     })
   }
 
@@ -37,7 +54,7 @@ export class RenseignementDonneesComponent implements OnInit {
       console.log(newLieu);
       this.formulaire.reset();
     });
-  
+  console.log("this formulaire")
   console.log(this.formulaire);
 }
 
